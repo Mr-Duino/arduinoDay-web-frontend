ede69884e8dcea67a87a0025af1b531e9492642b
WasathTheekshana <wasath.vt@gmail.com>
2023-03-01 23:29:37 +0530

    update packeges

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1 className='text-center font-semibold uppercase text-9xl text-3'>Arduino Day</h1>
+      <img src="/1.png" alt="" />
     </div>
   )
 }
---
+++ src/components/Teams.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Teams({}: Props) {
+  return (
+    <div>Teams</div>
+  )
+}
+
+export default Teams
